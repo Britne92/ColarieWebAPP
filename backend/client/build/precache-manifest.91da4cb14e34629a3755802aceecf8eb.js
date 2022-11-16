b34184c3e17e86b7e79aea66806311575e1ee421
YaunBrit <63310050+camilavalderrama@users.noreply.github.com>
2022-11-16 05:31:18 -0500

    Se agrego un build del frontend al back
    Paara producion

---
+++ backend/client/build/precache-manifest.91da4cb14e34629a3755802aceecf8eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0ee5c935709fec30288",
+    "url": "/static/css/main.4b8b6659.chunk.css"
+  },
+  {
+    "revision": "a0ee5c935709fec30288",
+    "url": "/static/js/main.a0ee5c93.chunk.js"
+  },
+  {
+    "revision": "9fec54145f66452735bf",
+    "url": "/static/css/1.b46ed13f.chunk.css"
+  },
+  {
+    "revision": "9fec54145f66452735bf",
+    "url": "/static/js/1.9fec5414.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9aa6092c4b6375d58005ce949fc1202",
+    "url": "/static/media/BarlowCondensed-Regular.b9aa6092.ttf"
+  },
+  {
+    "revision": "0e10c1ec77d433af69459aa5b3c6f43e",
+    "url": "/index.html"
+  }
+];
